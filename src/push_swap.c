00be345f642ce63192b4a011fc7166d1129f53da
iamdudumon <kksy4511@naver.com>
2024-07-13 15:07:54 +0900

    feat: 3-way merge_sort 기능 개발 중-6 (29 size를 제외한 sort 완료 -> 최적화 필요)

--- src/push_swap.c
+++ src/push_swap.c
@@ -81,10 +81,32 @@ static void	merge_sa_triangle(t_deque *sa, t_deque *sb, int size, int is_max)
 		}
 		if (node == sb->header && ts.s3)
 		{
-			if (size >= 3)
-				push(sa, sb, "pa\n");
+			push(sa, sb, "pa\n");
 			ts.s3--;
 		}
+	}
+}
+
+static void	merge_small_triangle(t_deque *sa, t_deque *sb, int size, int is_max, int is_a)
+{
+	t_size	ts;
+
+	ts = cal_size3(size);
+	if (is_a)
+	{
+		if (size == 1)
+			return ;
+		if (is_swap(sa, is_max))
+			swap(sa, "sa\n");
+	}
+	else
+	{
+		push(sb, sa, "pb\n");
+		if (size == 1)
+			return ;
+		push(sb, sa, "pb\n");
+		if (is_swap(sb, is_max))
+			swap(sb, "sb\n");
 	}
 }
 
@@ -92,30 +114,26 @@ static void	merge_sort(t_deque *sa, t_deque *sb, int size, int *cnts, int depth,
 {
 	t_size	ts;
 	int		cnt;
-	// printf("size: %d, depth: %d\n", size, depth);
+
 	ts = cal_size3(size);
 	cnt = cnts[depth];
 	if (size == 0)
 		return ;
-	// if (size <= 2)
-	// {
-	// 	if (is_a && is_swap(sa, is_max == 0))
-	// 		swap(sa, "sa\n");
-	// 	if (!is_a && is_swap(sb, is_max == 0))
-	// 		swap(sb, "sb\n");
-	// 	return ;
-	// }
-	if (size >= 2)
+	if (size >= 3)
 	{
 		merge_sort(sa, sb, ts.s1, cnts, depth + 1, is_max);
 		merge_sort(sa, sb, ts.s2, cnts, depth + 1, is_max);
 		merge_sort(sa, sb, ts.s3, cnts, depth + 1, is_max == 0);
 	}
-	// printf("size: %d, dep: %d\n", size, depth);
-	if ((depth + cnt + 1) % 2)
-		merge_sa_triangle(sa, sb, size, is_max);
+	if (size <= 2)
+		merge_small_triangle(sa, sb, size, is_max, (depth + cnt + 1) % 2);
 	else
-		merge_sb_triangle(sa, sb, size, is_max);
+	{
+		if ((depth + cnt + 1) % 2)
+			merge_sa_triangle(sa, sb, size, is_max);
+		else
+			merge_sb_triangle(sa, sb, size, is_max);
+	}
 	while (size-- && cnt % 3 != 2)
 	{
 		if ((depth + cnt + 1) % 2)
@@ -124,7 +142,6 @@ static void	merge_sort(t_deque *sa, t_deque *sb, int size, int *cnts, int depth,
 			rotate(sb, "rb\n");
 	}
 	cnts[depth]++;
-	// rotate_twin(sa, sb);
 }
 
 void	push_swap(t_deque *sa, t_deque *sb, int size)
