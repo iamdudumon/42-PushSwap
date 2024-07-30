49556d777ea52418749bbd4b4c560d4bba29e286
iamdudumon <kksy4511@naver.com>
2024-07-30 15:49:35 +0900

    fix: "" or "   " 등 공백이 들어와 stack a len이 0이면 Error 처리

--- src/main.c
+++ src/main.c
@@ -50,7 +50,7 @@ int	main(int argc, char *argv[])
 		return (0);
 	sa = generate_stack(argc, argv);
 	sb = generate_stack(0, 0);
-	if (!sa || !sb)
+	if (!sa || !sa->len || !sb)
 	{
 		ft_putstr_fd("Error\n", 1);
 		free_stack(sa, sb);
